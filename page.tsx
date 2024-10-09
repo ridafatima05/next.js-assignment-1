10291d7c64213a8bd31464485d04031a1efc188b
Rida Fatima <162724587+ridafatima05@users.noreply.github.com>
2024-10-09 18:20:20 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className='h-screen w-{100%} bg-orange-200'>This is my Home page</h1>
+      
+    </div>
+  )
+}
+
+export default Homepage
